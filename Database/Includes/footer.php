f6831829587ed92fbcd55bd5ff67f7f3f9520fed
rakhadeto <rakhadeto@gmail.com>
2025-06-29 23:49:01 +0700

    Create footer.php

---
+++ Database/Includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+            <p>&copy; 2025 To-Do List App. Dibuat dengan PHP dan MySQL.</p>
+        </footer>
+    </div>
+    <script src="assets/js/script.js"></script>
+</body>
+</html>

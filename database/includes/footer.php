29e4a0f7cd56d2434d70a463612fa3c040d558b9
Naufal Rakhadeto <rakhadeto@gmail.com>
2026-03-19 21:28:50 +0700

    Delete database/includes directory

--- database/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-            <p>&copy; 2025 To-Do List App. Dibuat dengan PHP dan MySQL.</p>
-        </footer>
-    </div>
-    <script src="assets/js/script.js"></script>
-</body>
-</html>
